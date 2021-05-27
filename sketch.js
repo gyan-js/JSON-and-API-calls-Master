9d2395150b2242bc568dedd86a4ab572fe9e81b5
GYAN-2008 <77232855+GYAN-2008@users.noreply.github.com>
2021-05-27 19:30:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,8 +23,7 @@ function setup(){
 
 function draw(){
      // add condition to check if any background image is there to add
-    if(backgroundImg)
-    background(backgroundImg);
+    
 
     Engine.update(engine);
     // write code to display time in correct format here
