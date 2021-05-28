29317dd5758333c75eca403b48f0ba10e0675729
GYAN-2008 <77232855+GYAN-2008@users.noreply.github.com>
2021-05-28 14:03:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,40 +45,40 @@ async function getBackgroundImg(){
 
     // add conditions to change the background images from sunrise to sunset
     if(hour>=04 && hour<=06 ){
-        debugger;
+       
         bg = "sunrise1.png";
     }else if(hour>=06 && hour<=08 ){
-        debugger;
+      
         bg = "sunrise2.png";
     }else if(hour>=08 && hour<=11 ){
-        debugger;
+      
         bg = "sunrise3.png";
     }else if(hour>=11 && hour<=13){
-        debugger;
+    
         bg = "sunrise4.png";
     }else if(hour>=13 && hour<=15){
-        debugger;
+        
         bg = "sunrise5.png";
     }else if(hour>=15 && hour<=17 ){
-        debugger;
+       
         bg = "sunrise6.png";
     }else if(hour>=17 && hour<=18 ){
         debugger;
         bg = "sunset7.png";
     }else if(hour>=18 && hour<=20 ){
-        debugger;
+       
         bg = "sunset8.png";
     }else if(hour>=20 && hour<=23 ){
-        debugger;
+       
         bg = "sunset9.png";
     }else if(hour>=23 && hour==0){
-        debugger;
+      
         bg = "sunset10.png";
     }else if(hour==0 && hour<=03){
-        debugger;
+      
         bg = "sunset11.png";
     }else{
-        debugger;
+     
         bg = "sunset12.png";
     }
 
